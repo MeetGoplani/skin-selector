772457e59d3b795e88c933d8190a3c145167b599
MeetGoplani <meet.goplani@nerdshouse.com>
2025-03-13 11:29:17 +0530

    add

--- src/home-page.jsx
+++ src/home-page.jsx
@@ -13,20 +13,22 @@ const MirariPromo = () => {
             className="w-16 h-16 md:w-48 md:h-48 fixed left-0 top-0"
           />
         </div>
-
+      
         <div className="flex flex-col sm:flex-row items-center space-y-2 sm:space-y-0 sm:space-x-6 md:space-x-16">
-          <p className="text-6xl md:text-8xl font-bold text-[#e50046] font-['Pastor_of_Muppets'] pt-10">
-            skins
-          </p>
-          <Link
+        <Link
             to="/skins"
            
           >
+            <p className="text-6xl md:text-8xl font-bold text-[#e50046] font-['Pastor_of_Muppets'] pt-10">
+            skins
+          </p>
+          </Link>
+          
             <img
               src="/images/logo2.png"
               className="w-full h-80  -my-24  md:w-48 "
             />
-          </Link>
+          
           <p className="text-6xl md:text-8xl font-bold text-[#e50046] font-['Pastor_of_Muppets'] pt-10">
             socials
           </p>
