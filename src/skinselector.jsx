ba5c085ec0191ff7943d593b75584b78d568a938
MeetGoplani <meet.goplani@nerdshouse.com>
2025-04-03 11:33:48 +0530

    add

--- src/skinselector.jsx
+++ src/skinselector.jsx
@@ -239,7 +239,7 @@ const SkinSelector = () => {
         if (skinId !== clickedItem) {
           setTimeout(() => {
             videoRef.pause();
-          }, 100); // Slightly longer timeout to ensure frame is displayed
+          }, 50); // Slightly longer timeout to ensure frame is displayed
         }
       }).catch((e) => {
         console.error("Initial video frame loading failed:", e);
